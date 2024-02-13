3a4a0d1fd4cd202d187a649e46ed8c2dc65f2fd6
Patrick Guio <p.guio@ucl.ac.uk>
2024-02-13 15:25:52 +0000

    Improved code example. Reordered files linking order to resolve issue with static structure member not being initialised prior to the registerMe constructor.

--- enum.cpp
+++ enum.cpp
@@ -3,27 +3,63 @@
 
 namespace valueTypes {
 
-  namespace {
+  namespace myBase {
+
+    const int none       = 0;
+    const int boolean    = 1;
+    const int character  = 2;
+    const int integer    = 3;
+    const int real       = 4;
+    const int charStr    = 5;
+    const int boolVect   = 6;
+    const int charVect   = 7;
+    const int intVect    = 8;
+    const int realVect   = 9;
+    const int stringVect = 10;
+
+
     valueMap::ValueDescPair v_mapInit[] = {
-      valueMap::ValueDescPair(none      , ""),
-      valueMap::ValueDescPair(boolean   , "bool"),
-      valueMap::ValueDescPair(character , "char"),
-      valueMap::ValueDescPair(integer   , "int"),
-      valueMap::ValueDescPair(real      , "real"),
-      valueMap::ValueDescPair(charStr   , "string"),
-      valueMap::ValueDescPair(boolVect  , "bool[]"),
-      valueMap::ValueDescPair(charVect  , "char[]"),
-      valueMap::ValueDescPair(intVect   , "int[]"),
-      valueMap::ValueDescPair(realVect  , "real[]"),
+      valueMap::ValueDescPair(none, ""),
+      valueMap::ValueDescPair(boolean, "bool"),
+      valueMap::ValueDescPair(character, "char"),
+      valueMap::ValueDescPair(integer, "int"),
+      valueMap::ValueDescPair(real, "real"),
+      valueMap::ValueDescPair(charStr, "string"),
+      valueMap::ValueDescPair(boolVect, "bool[]"),
+      valueMap::ValueDescPair(charVect, "char[]"),
+      valueMap::ValueDescPair(intVect, "int[]"),
+      valueMap::ValueDescPair(realVect, "real[]"),
       valueMap::ValueDescPair(stringVect, "string[]")
     };
+
     valueMap::ValueDescPair *v_mapInitEnd(v_mapInit+
-                                          sizeof v_mapInit / sizeof v_mapInit[0]);
+                                          sizeof(v_mapInit)/sizeof(v_mapInit[0]));
+
   }
+
   valueMap::valueMap(const Iter first, const Iter last)
   {
+	  std::cout << "valueMap constructor(first,last) with" << std::endl;
+	  Iter i = first;
+		do { 
+	  std::cout << i->first << " " << i->second << std::endl;
+		++i;
+		} while (i != last);
+    std::cout << "insert in values with" << std::endl;
+    printValueMap();
     values.insert(first, last);
+		std::cout << "inserted" << std::endl;
   }
-  valueMap::ValuesDescList valueMap::values(v_mapInit, v_mapInitEnd);
+
+	void valueMap::printValueMap()
+	{
+	  ValuesDescList::const_iterator i=values.begin();
+		do {
+      std::cout << i->first << " " << i->second << std::endl;
+      ++i;
+    } while (i != values.end());
+	}
+
 }
 
+valueTypes::valueMap::ValuesDescList valueTypes::valueMap::values(myBase::v_mapInit, myBase::v_mapInitEnd);
--- test-enum.cpp
+++ test-enum.cpp
@@ -2,12 +2,15 @@
 #include <enum.h>
 
 
-namespace valueTypes {
-  const static int boolVecR = 100;
-  const static int intVecR  = 101;
-  const static int realVecR = 102;
 
-  namespace {
+namespace valueTypes {
+
+namespace myExtra {
+
+  const int boolVecR = 100;
+  const int intVecR  = 101;
+  const int realVecR = 102;
+
     valueMap::ValueDescPair v_mapInit[] = {
       valueMap::ValueDescPair(boolVecR      , "bool[3]"),
       valueMap::ValueDescPair(intVecR       ,  "int[3]"),
@@ -15,22 +18,29 @@ namespace valueTypes {
     };
 
     valueMap::ValueDescPair *v_mapInitEnd(v_mapInit+
-                                          sizeof v_mapInit / sizeof v_mapInit[0]);
+                                          sizeof(v_mapInit)/sizeof(v_mapInit[0]));
 
-    valueMap registerMe(v_mapInit, v_mapInitEnd);
-  }
+		//valueMap::ValuesDescList valueMap::values(v_mapInit, v_mapInitEnd);
+		}
+		valueMap registerMe(myExtra::v_mapInit, myExtra::v_mapInitEnd);
+
 }
 
 using namespace valueTypes;
 
+
 int main()
 {
   {
+	  std::cout << "printValueMap() in main()" << std::endl;
+	  valueMap::printValueMap();
+/*
     valueMap::ValuesDescList::const_iterator i=valueMap::values.begin();
     do {
       std::cout << i->first << " " << i->second << std::endl;
       ++i;
     } while (i != valueMap::values.end());
+*/		
   }
   return 0;
 }
